1dcb1c3373a73e281b320be9561020d6e3216e90
Junior Gantin <nioperas06@gmail.com>
2019-07-12 06:15:59 +0100

    add package dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import os
 import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -13,6 +17,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/PythonBenin/kkiapay-python",
     packages=setuptools.find_packages(),
+    install_requires=required,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
