7f01cd512d1d206d92842a9e3a7c2e8a560b2d22
Junior Gantin <nioperas06@gmail.com>
2019-07-12 06:36:07 +0100

    add tests

--- tests/test_kkiapay.py
+++ tests/test_kkiapay.py
@@ -7,12 +7,21 @@ import requests_mock, requests
 def setup_module(module):
     """ setup any state specific to the execution of the given module."""
 
+
 def teardown_module(module):
     """ teardown any state that was previously setup with a setup_module
     method.
     """
 
 class TestKkiapay(object):
+
+    def test_urls(self):
+        live = Kkiapay('public_key', 'private_key', 'secret')
+        sandbox = Kkiapay('public_key', 'private_key', 'secret', sandbox=True)
+
+        assert live.url == Kkiapay.BASE_URL
+        assert sandbox.url == Kkiapay.SANDBOX_URL
+
 
     def test_answer(self, requests_mock):
         pass
