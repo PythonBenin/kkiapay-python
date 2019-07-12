4cd53cc73930933d42dcdfd4017d2dfe41f17e76
Junior Gantin <nioperas06@gmail.com>
2019-07-12 05:41:45 +0100

    add changes

--- test.py
+++
@@ -1,7 +0,0 @@
-from kkiapay import Kkiapay
-
-k = Kkiapay('b348e580a3c311e98ffb7b8677e53978', 'tpk_b3490c91a3c311e98ffb7b8677e53978', 'tsk_b3490c92a3c311e98ffb7b8677e53978', sandbox=True)
-
-transaction = k.verify_transaction('LVFNrK1nx')
-
-print(transaction)
